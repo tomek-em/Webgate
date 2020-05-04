65e20741e43d39171968ac8c918139bf3ab8b8d5
Tomek <tomek.mejer@gmail.com>
2020-05-04 21:12:04 +0200

    conf

--- conf/conf.php
+++ conf/conf.php
@@ -1,8 +1,8 @@
 <?php
     define('DB_HOST', 'localhost');
-    define('DB_USER', 'root_webgate');
-    define('DB_PASS', '1234pass');
-    define('DB_NAME', 'webgate');
+    define('DB_USER', '#');
+    define('DB_PASS', '#');
+    define('DB_NAME', '#');
 
     define('APP_NAME', 'WebGate');
     define('URL_ROOT', '/webgate/');
