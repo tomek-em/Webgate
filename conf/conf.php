7d09a403be31a0454f4fe7261ba882546c819455
tomek-em <45180736+tomek-em@users.noreply.github.com>
2020-07-09 08:25:07 +0200

    Update conf.php

--- conf/conf.php
+++ conf/conf.php
@@ -1,7 +1,7 @@
 <?php
     define('DB_HOST', 'localhost');
     define('DB_USER', 'root_webgate');
-    define('DB_PASS', '1234pass');
+    define('DB_PASS', 'xxx');
     define('DB_NAME', 'webgate');
 
     define('APP_NAME', 'WebGate');
